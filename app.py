4c1c0acd0f4afb329627018535c67c496bec66a0
ParasSondhi <121051995+ParasSondhi@users.noreply.github.com>
2026-04-03 05:03:38 +0530

    used embed to display pdf

--- app.py
+++ app.py
@@ -127,7 +127,7 @@ elif st.session_state.step == 3:
         # --- NEW: PDF PREVIEW ---
         st.write("### Document Preview")
         base64_pdf = base64.b64encode(pdf_bytes).decode('utf-8')
-        pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="100%" height="800" type="application/pdf"></iframe>'
+        pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="100%" height="800" type="application/pdf"></iframe>'
         st.markdown(pdf_display, unsafe_allow_html=True)
         # ------------------------
         
