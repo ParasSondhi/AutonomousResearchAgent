5e882874f06e1e5c9348bb45be42adce527d2fbb
ParasSondhi <121051995+ParasSondhi@users.noreply.github.com>
2026-04-03 07:01:24 +0530

    made some changes in pdf display

--- app.py
+++ app.py
@@ -136,7 +136,7 @@ elif st.session_state.step == 3:
         # --- NEW: PDF PREVIEW ---
         st.write("### Document Preview")
         base64_pdf = base64.b64encode(pdf_bytes).decode('utf-8')
-        pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="100%" height="800" type="application/pdf">'
+        pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="100%" height="800" type="application/pdf"></embed>'
         st.markdown(pdf_display, unsafe_allow_html=True)
         # ------------------------
         
